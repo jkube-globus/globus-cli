77eb22fe5767e74989d0a8adb43bc860d03d6a42
Stephen Rosen <sirosen@globus.org>
2022-10-13 12:26:31 -0400

    Bump SDK to latest and refine update_dependencies (#685)
    `scripts/update_dependencies.py` now supports "arbitrary" packages
    with a simple CLI flag to select packages. This bundles the mypy
    updating and globus-sdk updating into a single script.
    
    Using this, bump to globus-sdk==3.13.0

--- scripts/update_dependencies.py
+++ scripts/update_dependencies.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from __future__ import annotations
 
+import argparse
 import json
 import pathlib
 import re
@@ -13,24 +14,29 @@ class Abort(RuntimeError):
     pass
 
 
-def get_sdk_latest() -> str:
-    with urllib.request.urlopen("https://pypi.python.org/pypi/globus-sdk/json") as conn:
+def get_pkg_latest(name: str) -> str:
+    with urllib.request.urlopen(f"https://pypi.python.org/pypi/{name}/json") as conn:
         version_data = json.load(conn)
     return str(version_data["info"]["version"])
 
 
-def bump_sdk_version_on_file(path: pathlib.Path, new_version: str) -> None:
-    print(f"updating globus-sdk in {path.relative_to(REPO_ROOT)} ... ", end="")
+def bump_pkg_version_on_file(
+    path: pathlib.Path,
+    pkg_name: str,
+    new_version: str,
+    *,
+    version_format: str = r"(\d+\.\d+\.\d+)",
+) -> None:
+    print(f"updating {pkg_name} in {path.relative_to(REPO_ROOT)} ... ", end="")
     with open(path) as fp:
         content = fp.read()
-    match = re.search(r"globus-sdk==(\d+\.\d+\.\d+)", content)
+    match = re.search(re.escape(pkg_name) + "==" + version_format, content)
     if not match:
-        print("fail (abort)")
-        raise Abort(f"{path} did not contain sdk version pattern")
+        raise Abort(f"{path} did not contain {pkg_name} version pattern")
 
     old_version = match.group(1)
-    old_str = f"globus-sdk=={old_version}"
-    new_str = f"globus-sdk=={new_version}"
+    old_str = f"{pkg_name}=={old_version}"
+    new_str = f"{pkg_name}=={new_version}"
     content = content.replace(old_str, new_str)
     with open(path, "w") as fp:
         fp.write(content)
@@ -38,13 +44,34 @@ def bump_sdk_version_on_file(path: pathlib.Path, new_version: str) -> None:
 
 
 def bump_sdk_version() -> None:
-    new_version = get_sdk_latest()
-    bump_sdk_version_on_file(REPO_ROOT / "setup.py", new_version)
-    bump_sdk_version_on_file(REPO_ROOT / ".pre-commit-config.yaml", new_version)
+    sdk_version = get_pkg_latest("globus-sdk")
+    bump_pkg_version_on_file(REPO_ROOT / "setup.py", "globus-sdk", sdk_version)
+
+
+def bump_mypy_version() -> None:
+    mypy_version = get_pkg_latest("mypy")
+    bump_pkg_version_on_file(
+        REPO_ROOT / "tox.ini", "mypy", mypy_version, version_format=r"(\d+\.\d+)"
+    )
 
 
 def main() -> None:
-    bump_sdk_version()
+    parser = argparse.ArgumentParser()
+    _all_pkgs = ("sdk", "mypy")
+    parser.add_argument(
+        "--pkg",
+        choices=_all_pkgs,
+        action="append",
+        help="select a dependency to update, defaults to all",
+    )
+    args = parser.parse_args()
+
+    pkgs = args.pkg or _all_pkgs
+
+    if "sdk" in pkgs:
+        bump_sdk_version()
+    if "mypy" in pkgs:
+        bump_mypy_version()
 
 
 if __name__ == "__main__":
