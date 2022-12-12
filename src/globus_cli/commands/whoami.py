84affa323ed9443bd5f0cc319bd36e75f471c9fe
Stephen Rosen <sirosen@globus.org>
2022-12-12 15:17:49 -0600

    Type annotate the whoami command (#726)
    Remove it from `mypy`'s list of `disallow_untyped_defs = false`, then
    fix that check. It needs a return type (None).

--- src/globus_cli/commands/whoami.py
+++ src/globus_cli/commands/whoami.py
@@ -49,7 +49,7 @@ $ globus whoami --linked-identities
     help="Also show identities linked to the currently logged-in primary identity.",
 )
 @LoginManager.requires_login(LoginManager.AUTH_RS)
-def whoami_command(*, login_manager: LoginManager, linked_identities: bool):
+def whoami_command(*, login_manager: LoginManager, linked_identities: bool) -> None:
     """
     Display information for the currently logged-in user.
     """
