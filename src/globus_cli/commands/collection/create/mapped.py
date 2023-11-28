421bacd05ad81bb9988c2d5f4db764e993276eb0
Stephen Rosen <sirosen@globus.org>
2023-11-28 17:14:12 -0600

    Update 'collection create mapped' after rebase
    Adjust option declarations, ensure they pass mypy and the
    click-type-test integration, and update the phrasing in many small
    ways to match the current expectations and usages of the CLI.

--- src/globus_cli/commands/collection/create/mapped.py
+++ src/globus_cli/commands/collection/create/mapped.py
@@ -10,238 +10,191 @@ from globus_cli.constants import ExplicitNullType
 from globus_cli.login_manager import LoginManager
 from globus_cli.parsing import (
     JSONStringOrFile,
-    StringOrNull,
-    UrlOrNull,
+    ParsedJSONData,
     command,
     endpoint_id_arg,
     endpointish_params,
-    mutex_option_group,
-    nullable_multi_callback,
 )
 from globus_cli.termio import Field, TextMode, display
 
 
-def collection_create_params(f):
-    """
-    Collection of options consumed by GCS Collection create.
-    """
-    multi_use_option_str = "Give this option multiple times in a single command"
-    f = click.argument("BASE_PATH")(f)
-
-    f = click.option(
-        "--storage-gateway",
-        help=(
-            "The storage gateway ID to host this collection. "
-            "If no value is provided but the endpoint has exactly one gateway, "
-            "that gateway will be used by default."
-        ),
-    )(f)
-    f = click.option(
-        "--public/--private",
-        "public",
-        default=True,
-        help="Set the collection to be public or private",
-    )(f)
-    f = click.option(
-        "--sharing-restrict-paths",
-        type=JSONStringOrFile(null="null"),
-        help="Path restrictions for sharing data on guest collections "
-        "based on this collection. This option is only usable on Mapped "
-        "Collections",
-    )(f)
-    f = click.option(
-        "--allow-guest-collections/--no-allow-guest-collections",
-        default=None,
-        help=(
-            "Allow Guest Collections to be created on this Collection. This option "
-            "is only usable on Mapped Collections. If this option is disabled on a "
-            "Mapped Collection which already has associated Guest Collections, "
-            "those collections will no longer be accessible"
-        ),
-    )(f)
-    f = click.option(
-        "--disable-anonymous-writes/--enable-anonymous-writes",
-        default=None,
-        help=(
-            "Allow anonymous write ACLs on Guest Collections attached to this "
-            "Mapped Collection. This option is only usable on non high assurance "
-            "Mapped Collections and the setting is inherited by the hosted Guest "
-            "Collections. Anonymous write ACLs are enabled by default "
-            "(requires an endpoint with API v1.8.0)"
-        ),
-    )(f)
-    f = click.option(
-        "--domain-name",
-        help=(
-            "DNS host name for the collection (mapped "
-            "collections only). This may be either a host name "
-            "or a fully-qualified domain name, but if it is the latter "
-            "it must be a subdomain of the endpoint's domain"
-        ),
-    )(f)
-
-    f = click.option(
-        "--enable-https",
-        is_flag=True,
-        help=(
-            "Explicitly enable HTTPS supprt (requires a managed endpoint "
-            "with API v1.1.0)"
-        ),
-    )(f)
-    f = click.option(
-        "--disable-https",
-        is_flag=True,
-        help=(
-            "Explicitly disable HTTPS supprt (requires a managed endpoint "
-            "with API v1.1.0)"
-        ),
-    )(f)
-    f = mutex_option_group("--enable-https", "--disable-https")(f)
-
-    f = click.option(
-        "--user-message",
-        help=(
-            "A message for clients to display to users when interacting "
-            "with this collection"
-        ),
-        type=StringOrNull(),
-    )(f)
-    f = click.option(
-        "--user-message-link",
-        help=(
-            "Link to additional messaging for clients to display to users "
-            "when interacting with this endpoint, linked to an http or https URL "
-            "with this collection"
-        ),
-        type=UrlOrNull(),
-    )(f)
-    f = click.option(
-        "--sharing-user-allow",
-        "sharing_users_allow",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "Connector-specific username allowed to create guest collections."
-            f"{multi_use_option_str} to allow multiple users. "
-            'Set a value of "" to clear this'
-        ),
-    )(f)
-    f = click.option(
-        "--sharing-user-deny",
-        "sharing_users_deny",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "Connector-specific username denied permission to create guest "
-            f"collections. {multi_use_option_str} to deny multiple users. "
-            'Set a value of "" to clear this'
-        ),
-    )(f)
-    f = click.option(
-        "--posix-sharing-group-allow",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "POSIX group allowed access to create guest collections "
-            "(POSIX Connector Only). "
-            f"{multi_use_option_str} to allow multiple groups. "
-            'Set a value of "" to clear this'
-        ),
-    )(f)
-    f = click.option(
-        "--posix-sharing-group-deny",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "POSIX group denied permission to create guest collections "
-            "(POSIX Connector Only). "
-            f"{multi_use_option_str} to deny multiple groups. "
-            + 'Set a value of "" to clear this'
-        ),
-    )(f)
-    f = click.option(
-        "--google-project-id",
-        help=(
-            "For Google Cloud Storage backed Collections only. The Google "
-            "Cloud Platform project ID which is used by this Collection"
-        ),
-        type=StringOrNull(),
-    )(f)
-
-    # POSIX Staging connector (GCS v5.4.10)
-    f = click.option(
-        "--posix-staging-sharing-group-allow",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "POSIX group allowed access to create guest collections "
-            "(POSIX Staging Connector Only). "
-            f"{multi_use_option_str} to allow multiple groups. "
-            'Set a value of "" to clear this'
-        ),
-    )(f)
-    f = click.option(
-        "--posix-staging-sharing-group-deny",
-        multiple=True,
-        callback=nullable_multi_callback(""),
-        help=(
-            "POSIX group denied permission to create guest collections "
-            "(POSIX Staging Connector Only). "
-            f"{multi_use_option_str} to deny multiple groups. "
-            'Set a value of "" to clear this'
-        ),
-    )(f)
-    return f
+def _make_multi_use_option_str(s: str) -> str:
+    return f"Give this option multiple times to {s}."
 
 
 @command("create", short_help="Create a new Mapped Collection")
 @endpoint_id_arg
-@endpointish_params.create(name="collection")
-@collection_create_params
+@click.option(
+    "--base-path",
+    default="/",
+    show_default=True,
+    help="The location within the storage gateway where the collection is rooted.",
+)
+@click.option(
+    "--storage-gateway",
+    help=(
+        "The storage gateway ID to host this collection. "
+        "If no value is provided but the endpoint has exactly one gateway, "
+        "that gateway will be used by default."
+    ),
+)
+@click.option(
+    "--public/--private",
+    "public",
+    default=True,
+    help="Set the collection to be public or private.",
+)
+@click.option(
+    "--sharing-restrict-paths",
+    type=JSONStringOrFile(null="null"),
+    help=(
+        "Path restrictions for sharing data on guest collections "
+        "based on this collection."
+    ),
+)
+@click.option(
+    "--allow-guest-collections/--no-allow-guest-collections",
+    default=None,
+    help=(
+        "Allow Guest Collections to be created on this Collection. "
+        "If this option is later disabled on a Mapped Collection which has associated "
+        "Guest Collections, those collections will no longer be accessible."
+    ),
+)
+@click.option(
+    "--disable-anonymous-writes/--enable-anonymous-writes",
+    default=None,
+    help=(
+        "Allow anonymous write ACLs on Guest Collections attached to this "
+        "Mapped Collection. This option is only usable on non high assurance "
+        "Mapped Collections and the setting is inherited by the hosted Guest "
+        "Collections. Anonymous write ACLs are enabled by default "
+        "(requires an endpoint with API v1.8.0)."
+    ),
+)
+@click.option(
+    "--domain-name",
+    help=(
+        "DNS host name for the collection. This may be either a host name "
+        "or a fully-qualified domain name, but if it is the latter "
+        "it must be a subdomain of the endpoint's domain."
+    ),
+)
 @click.option(
     "--identity-id",
     help=(
         "Globus Auth identity to who acts as the owner of this Collection. This "
-        "identity must be an administrator on the Endpoint"
+        "identity must be an administrator on the Endpoint."
     ),
 )
-@LoginManager.requires_login(LoginManager.TRANSFER_RS, LoginManager.AUTH_RS)
+@click.option(
+    "--enable-https/--disable-https",
+    "enable_https",
+    default=None,
+    help=(
+        "Explicitly enable or disable  HTTPS support (requires a managed endpoint "
+        "with API v1.1.0)"
+    ),
+)
+@click.option(
+    "--sharing-user-allow",
+    "sharing_users_allow",
+    multiple=True,
+    help=(
+        "Connector-specific username allowed to create guest collections."
+        + _make_multi_use_option_str("allow multiple users")
+    ),
+)
+@click.option(
+    "--sharing-user-deny",
+    "sharing_users_deny",
+    multiple=True,
+    help=(
+        "Connector-specific username denied permission to create guest collections. "
+        + _make_multi_use_option_str("deny multiple users")
+    ),
+)
+@click.option(
+    "--posix-sharing-group-allow",
+    multiple=True,
+    help=(
+        "POSIX group allowed access to create guest collections "
+        + "(POSIX Connector only). "
+        + _make_multi_use_option_str("allow multiple groups")
+    ),
+)
+@click.option(
+    "--posix-sharing-group-deny",
+    multiple=True,
+    help=(
+        "POSIX group denied permission to create guest collections "
+        + "(POSIX Connector only). "
+        + _make_multi_use_option_str("deny multiple groups")
+    ),
+)
+@click.option(
+    "--google-project-id",
+    help=(
+        "The Google Cloud Platform project ID which is used by this Collection "
+        "(Google Cloud Storage backed Collections only)."
+    ),
+)
+@click.option(
+    # POSIX Staging connector (GCS v5.4.10)
+    "--posix-staging-sharing-group-allow",
+    multiple=True,
+    help=(
+        "POSIX group allowed access to create guest collections "
+        + "(POSIX Staging Connector Only). "
+        + _make_multi_use_option_str("allow multiple groups")
+    ),
+)
+@click.option(
+    "--posix-staging-sharing-group-deny",
+    multiple=True,
+    help=(
+        "POSIX group denied permission to create guest collections "
+        + "(POSIX Staging Connector Only). "
+        + _make_multi_use_option_str("deny multiple groups")
+    ),
+)
+@endpointish_params.create(name="collection")
+@LoginManager.requires_login("auth", "transfer")
 def collection_create_mapped(
-    *,
     login_manager: LoginManager,
+    *,
     # positional args
-    endpoint_id: uuid.UUID,
     base_path: str,
     display_name: str,
+    endpoint_id: uuid.UUID,
     # options
-    storage_gateway: str | None,
-    public: bool,
-    description: str | None,
-    info_link: str | None,
-    contact_info: str | None,
-    contact_email: str | None,
-    organization: str | None,
-    department: str | None,
-    keywords: list[str] | None,
-    default_directory: str | None,
-    force_encryption: bool | None,
-    domain_name: str | None,
-    disable_anonymous_writes: bool | None,
-    identity_id: str | None,
-    google_project_id: str | None,
-    posix_sharing_group_allow: t.Iterable[str] | None,
-    posix_sharing_group_deny: t.Iterable[str] | None,
-    posix_staging_sharing_group_allow: t.Iterable[str] | None,
-    posix_staging_sharing_group_deny: t.Iterable[str] | None,
-    sharing_restrict_paths: t.Dict[str, t.Iterable[str]] | None | ExplicitNullType,
     allow_guest_collections: bool | None,
-    sharing_users_allow: t.Iterable[str] | None,
-    sharing_users_deny: t.Iterable[str] | None,
+    contact_email: str | None | ExplicitNullType,
+    contact_info: str | None | ExplicitNullType,
+    default_directory: str | None | ExplicitNullType,
+    department: str | None | ExplicitNullType,
+    description: str | None | ExplicitNullType,
+    disable_anonymous_writes: bool | None,
+    domain_name: str | None,
+    enable_https: bool | None,
+    force_encryption: bool | None,
+    google_project_id: str | None,
+    identity_id: str | None,
+    info_link: str | None | ExplicitNullType,
+    keywords: list[str] | None,
+    organization: str | None | ExplicitNullType,
+    posix_sharing_group_allow: tuple[str, ...],
+    posix_sharing_group_deny: tuple[str, ...],
+    posix_staging_sharing_group_allow: tuple[str, ...],
+    posix_staging_sharing_group_deny: tuple[str, ...],
+    public: bool,
+    sharing_restrict_paths: ParsedJSONData | None | ExplicitNullType,
+    sharing_users_allow: tuple[str, ...],
+    sharing_users_deny: tuple[str, ...],
+    storage_gateway: str | None,
     user_message: str | None | ExplicitNullType,
     user_message_link: str | None | ExplicitNullType,
-    enable_https: bool | None,
-    disable_https: bool | None,
-    verify: str | None,
+    verify: dict[str, bool],
 ) -> None:
     """
     Create a new Mapped Collection, rooted on some given path within an
@@ -251,6 +204,11 @@ def collection_create_mapped(
     and there is only one storage gateway on the endpoint, that gateway will be used.
     Otherwise, '--storage-gateway' is required.
     """
+    if isinstance(sharing_restrict_paths, ParsedJSONData) and not isinstance(
+        sharing_restrict_paths.data, dict
+    ):
+        raise click.UsageError("--sharing-restrict-paths must be a JSON object")
+
     gcs_client = login_manager.get_gcs_client(endpoint_id=endpoint_id)
     if storage_gateway is not None:
         storage_gateway_id = storage_gateway
@@ -269,78 +227,68 @@ def collection_create_mapped(
                 "You must specify which one to use with the --storage-gateway option."
             )
 
-    policies: t.Optional[globus_sdk.CollectionPolicies] = None
+    policies: globus_sdk.CollectionPolicies | None = None
     if google_project_id is not None:
         policies = globus_sdk.GoogleCloudStorageCollectionPolicies(
             project=google_project_id
         )
 
-    if posix_sharing_group_allow is not None or posix_sharing_group_deny is not None:
+    if posix_sharing_group_allow or posix_sharing_group_deny:
         if policies is not None:
-            raise click.UsageError("Incompatible policy options detected")
+            raise click.UsageError("Incompatible policy options detected.")
         # Added in 5.4.8 for POSIX connector
         policies = globus_sdk.POSIXCollectionPolicies(
             sharing_groups_allow=posix_sharing_group_allow,
             sharing_groups_deny=posix_sharing_group_deny,
         )
 
-    if (
-        posix_staging_sharing_group_allow is not None
-        or posix_staging_sharing_group_deny is not None
-    ):
+    if posix_staging_sharing_group_allow or posix_staging_sharing_group_deny:
         if policies is not None:
-            raise click.UsageError("Incompatible policy options detected")
+            raise click.UsageError("Incompatible policy options detected.")
         # Added in 5.4.10 for POSIX staging connector
         policies = globus_sdk.POSIXStagingCollectionPolicies(
             sharing_groups_allow=posix_staging_sharing_group_allow,
             sharing_groups_deny=posix_staging_sharing_group_deny,
         )
 
-    # These are added in GCS 5.4.4, so if neither --enable-https or
-    # --disable-https are set, then we'll not try to set the value for
-    # backward compatibility
-    if enable_https is False:
-        enable_https = None
-    if disable_https:
-        enable_https = False
-
-    force_verify: bool | None = None
-    disable_verify: bool | None = None
-    if verify is not None:
-        if verify.lower() == "force":
-            force_verify, disable_verify = True, False
-        elif verify.lower() == "disable":
-            force_verify, disable_verify = False, True
-        else:
-            force_verify, disable_verify = False, False
+    converted_kwargs: dict[str, t.Any] = ExplicitNullType.nullify_dict(
+        {
+            # required arguments
+            "collection_base_path": base_path,
+            "display_name": display_name,
+            # options
+            "allow_guest_collections": allow_guest_collections,
+            "contact_email": contact_email,
+            "contact_info": contact_info,
+            "default_directory": default_directory,
+            "department": department,
+            "description": description,
+            "disable_anonymous_writes": disable_anonymous_writes,
+            "domain_name": domain_name,
+            "enable_https": enable_https,
+            "force_encryption": force_encryption,
+            "identity_id": identity_id,
+            "info_link": info_link,
+            "keywords": keywords,
+            "organization": organization,
+            "policies": policies,
+            "public": public,
+            "sharing_restrict_paths": (
+                sharing_restrict_paths.data
+                if isinstance(sharing_restrict_paths, ParsedJSONData)
+                else sharing_restrict_paths
+            ),
+            "sharing_users_allow": sharing_users_allow,
+            "sharing_users_deny": sharing_users_deny,
+            "storage_gateway_id": storage_gateway_id,
+            "user_message": user_message,
+            "user_message_link": user_message_link,
+        }
+    )
+    converted_kwargs.update(verify)
 
     collection_doc = globus_sdk.MappedCollectionDocument(
-        storage_gateway_id=storage_gateway_id,
-        collection_base_path=base_path,
-        display_name=display_name,
-        public=public,
-        description=description,
-        info_link=info_link,
-        contact_info=contact_info,
-        contact_email=contact_email,
-        organization=organization,
-        department=department,
-        keywords=keywords,
-        default_directory=default_directory,
-        force_encryption=force_encryption,
-        domain_name=domain_name,
-        disable_anonymous_writes=disable_anonymous_writes,
-        identity_id=identity_id,
-        policies=policies,
-        allow_guest_collections=allow_guest_collections,
-        sharing_users_allow=sharing_users_allow,
-        sharing_users_deny=sharing_users_deny,
-        sharing_restrict_paths=ExplicitNullType.nullify(sharing_restrict_paths),
-        user_message=ExplicitNullType.nullify(user_message),
-        user_message_link=ExplicitNullType.nullify(user_message_link),
-        enable_https=enable_https,
-        disable_verify=disable_verify,
-        force_verify=force_verify,
+        **converted_kwargs,
     )
     res = gcs_client.create_collection(collection_doc)
 
