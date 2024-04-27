22fd01522e6399c8619781c6e09fcbbe97a1a1a8
Stephen Rosen <sirosen@globus.org>
2024-04-27 16:40:01 -0500

    Fix a regression in handling of EPIPE
    In click 7.0, a body of work introduced some specialty EPIPE handling.
    At the time, we picked up the benefit and it worked under several test
    scenarios. click's handling has not changed, but at some point we
    updated the way we were passing exceptions to our custom excepthook.
    In the course of that work, we accidentally broke the EPIPE handling,
    which expects the error to ascend from `Command.invoke()` to
    `Command.main()`.
    
    We have no current need to invoke our excepthooks on OSError of any
    flavor, so as the immediate fix, we can start reraising those
    immediately.
    
    In the future, we could refine this to only reraise on EPIPE,
    specifically, if necessary. The unit test for this behavior specifies
    EPIPE, to be robust to such an improvement.
    
    The error can be verified and confirmed fixed with a usage like
    
        globus endpoint search tutorial | head -n1
    
    Prior to this fix, broken pipe errors are sent to stderr. With the
    fix, they do not appear.

--- tests/unit/test_command_decorators.py
+++ tests/unit/test_command_decorators.py
@@ -1,6 +1,36 @@
+import errno
+from unittest import mock
+
 import click
 
-from globus_cli.parsing import command
+from globus_cli.parsing import command, main_group
+
+
+def test_main_group_is_always_named_globus():
+    @main_group()
+    def foo():
+        pass
+
+    assert foo.name == "globus"
+
+
+def test_main_group_reraises_epipe_errors_without_invoking_custom_handler(runner):
+    @main_group()
+    def foo():
+        click.echo("hi")
+        # simulate a broken pipe, as would happen if a command is piped to a command
+        # which only reads some of the output before closing the stream, like `head`
+        raise OSError("broken pipe", errno=errno.EPIPE)
+
+    # we want to assert not only that the command exits successfully, but specifically
+    # that it does not invoke the CLI's custom exception handler
+    #
+    # if it does, that means we won't get click's cleanup for EPIPE, which includes
+    # special wrapping of `sys.stdout` and `sys.stderr`
+    with mock.patch("globus_cli.parsing.commands.custom_except_hook") as mock_handler:
+        result = runner.invoke(foo, [])
+    assert result.exit_code == 0
+    assert mock_handler.call_count == 0
 
 
 def test_custom_command_missing_param_helptext(runner):
