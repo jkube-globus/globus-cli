5a705b148cc8090f49c9279f66982206c2386add
Stephen Rosen <sirosen@globus.org>
2025-03-25 17:27:19 -0500

    Update SDK + use 'SearchClient.index_list()' (#1091)
    - run scripts/update_dependencies.py
    - update to `index_list()` in `globus search index list`

--- src/globus_cli/commands/search/index/list.py
+++ src/globus_cli/commands/search/index/list.py
@@ -15,8 +15,5 @@ def list_command(login_manager: LoginManager) -> None:
     """List indices where you have some permissions."""
     search_client = login_manager.get_search_client()
     display(
-        search_client.get("/v1/index_list"),
-        fields=INDEX_LIST_FIELDS,
-        text_mode=display.TABLE,
-        response_key="index_list",
+        search_client.index_list(), fields=INDEX_LIST_FIELDS, text_mode=display.TABLE
     )
