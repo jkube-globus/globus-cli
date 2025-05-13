42ac064234aa90d002e6163d63263b97c5745c81
Stephen Rosen <sirosen@globus.org>
2025-05-13 13:56:33 -0500

    Fix a bug in _click_compat: ctx=None
    Trying to use `click.get_current_context()` for get_metavar is unsafe
    on older click versions because the function can be called when there
    is no current context pushed. Instead of trying hard to resolve this,
    simply pass `ctx=None` and allow for the type violation.

--- src/globus_cli/_click_compat.py
+++ src/globus_cli/_click_compat.py
@@ -22,14 +22,18 @@ def shim_get_metavar(f: C) -> C:
     the 8.2.0+ APIs.
 
     Under 8.2.0, `ctx: click.Context` is passed, while older versions do not.
-    Therefore, do nothing on 8.2.0+ and pass `click.get_current_context()' if
-    the older click version is in use.
+    Therefore, do nothing on 8.2.0+ and pass `ctx=None' if the older click
+    version is in use.
+
+    NOTE: we pass `ctx=None` which violates the declared types (but works at
+    runtime) because when running under older click versions, there may not be
+    a current click context.
     """
     if OLDER_CLICK_API:
 
         @functools.wraps(f)
         def wrapper(*args: t.Any, **kwargs: t.Any) -> t.Any:
-            return f(*args, **kwargs, ctx=click.get_current_context())
+            return f(*args, **kwargs, ctx=None)
 
         return wrapper  # type: ignore[return-value]
 
