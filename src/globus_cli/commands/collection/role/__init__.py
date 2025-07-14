f9ad68c5872976d02e40d285ccaf8ee8279cf97b
Stephen Rosen <sirosen@globus.org>
2025-07-14 14:45:10 -0500

    Add a collection role show command
    Introduce a new command to display a role.
    
    Add a changelog fragment for it.

--- src/globus_cli/commands/collection/role/__init__.py
+++ src/globus_cli/commands/collection/role/__init__.py
@@ -3,7 +3,9 @@ from globus_cli.parsing import group
 
 @group(
     "role",
-    lazy_subcommands={},
+    lazy_subcommands={
+        "show": (".show", "show_command"),
+    },
 )
 def role_command() -> None:
     """Manage Roles on Collections."""
