cc78bfa3c0d196e33cd74cdefba470acc3944bb0
Stephen Rosen <sirosen@globus.org>
2025-07-17 20:57:01 -0500

    Adapt a nontrivial test to use GetIdentitiesMocker

--- tests/functional/test_session_show.py
+++ tests/functional/test_session_show.py
@@ -11,48 +11,31 @@ class utconly_datetime(datetime.datetime):
 
 
 @pytest.fixture(autouse=True, scope="session")
-def _register_session_introspect_response():
-    cthulhu_user_attrs = {
+def setup_session():
+    user_attrs = {
         "username": "cthulhu@rlyeh",
         "name": "Cthulhu",
         "email": "dead-cthulhu-waits-dreaming@globus.org",
         "id": str(uuid.uuid4()),
-        "idp_id": str(uuid.uuid4()),
-        "auth_time": "1989-06-07 10:11 UTC",
+        "identity_provider": str(uuid.uuid4()),
+        "organization": "Point Nemo",
     }
-
+    auth_time = "1989-06-07 10:11 UTC"
     register_response_set(
-        "cthulhu_session",
+        "cthulhu_introspect",
         {
             "introspect": {
                 "service": "auth",
                 "path": "/v2/oauth2/token/introspect",
                 "method": "POST",
-                "json": create_introspect_data(cthulhu_user_attrs),
-            },
-            "identities": {
-                "service": "auth",
-                "path": "/v2/api/identities",
-                "json": {
-                    "identities": [
-                        {
-                            "username": cthulhu_user_attrs["username"],
-                            "name": cthulhu_user_attrs["name"],
-                            "id": cthulhu_user_attrs["id"],
-                            "identity_provider": cthulhu_user_attrs["idp_id"],
-                            "organization": "Point Nemo",
-                            "status": "used",
-                            "email": cthulhu_user_attrs["email"],
-                        }
-                    ]
-                },
+                "json": create_introspect_data(user_attrs, auth_time=auth_time),
             },
         },
-        metadata=cthulhu_user_attrs,
+        metadata={"user_attrs": user_attrs, "auth_time": auth_time},
     )
 
 
-def create_introspect_data(user_attrs):
+def create_introspect_data(user_attrs, auth_time):
     # NB: this is supposed to match the client ID for the generated CLI client
     # however, nothing checks it today
     # revisit if we add checks to our commands for this, as we'll need to ensure that
@@ -83,9 +66,9 @@ def create_introspect_data(user_attrs):
                 user_attrs["id"]: {
                     "acr": None,
                     "amr": None,
-                    "idp": user_attrs["idp_id"],
+                    "idp": user_attrs["identity_provider"],
                     "auth_time": datetime.datetime.strptime(
-                        user_attrs["auth_time"], "%Y-%m-%d %H:%M %Z"
+                        auth_time, "%Y-%m-%d %H:%M %Z"
                     )
                     .replace(tzinfo=datetime.timezone.utc)
                     .timestamp(),
@@ -96,8 +79,9 @@ def create_introspect_data(user_attrs):
     }
 
 
-def test_session_show_text(run_line, monkeypatch):
-    meta = load_response_set("cthulhu_session").metadata
+def test_session_show_text(run_line, monkeypatch, get_identities_mocker):
+    meta = load_response_set("cthulhu_introspect").metadata
+    get_identities_mocker.setup_one_identity(**meta["user_attrs"])
 
     monkeypatch.setattr(datetime, "datetime", utconly_datetime)
 
@@ -110,6 +94,6 @@ def test_session_show_text(run_line, monkeypatch):
     lines = lines[:-1]
     content_row = [x.strip() for x in lines[-1].split("|")]
     assert len(content_row) == 3  # 3 columns
-    assert content_row[0] == meta["username"]
-    assert content_row[1] == meta["id"]
+    assert content_row[0] == meta["user_attrs"]["username"]
+    assert content_row[1] == meta["user_attrs"]["id"]
     assert content_row[2] == meta["auth_time"]
