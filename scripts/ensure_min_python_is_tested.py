8886e8de008048a0e950b2bdc30739d8853d8d7f
Stephen Rosen <sirosen@globus.org>
2025-08-04 14:47:39 -0500

    Add a script for confirming min python is tested
    Based on the version of this script in `globus-sdk`, this change is a
    preparatory move for removal of Python 3.8 support. When we do this,
    we want to make sure we keep the minimal version of Python
    well-aligned in the repo.
    
    Use `mddj` to read the minimum version of Python from package
    metadata and the minimum listed version in tox environments.
    Use YAML parsing to pull the relevant data from GitHub workflows.
    
    Add a tox environment + a CI check which runs it to call the script.
    
    In dependency-groups data, `yaml` is now a group so that we can keep a
    single pinned version for both testsuite usage and the new
    `check-project-metadata` group. This group now also supplies the
    dependencies for the `build`+`twine check` tox environment.

---
+++ scripts/ensure_min_python_is_tested.py
@@ -0,0 +1,60 @@
+# run via `tox r -e check-min-python-is-tested`
+import pathlib
+import subprocess
+import sys
+
+import ruamel.yaml
+
+YAML = ruamel.yaml.YAML(typ="safe")
+REPO_ROOT = pathlib.Path(__file__).parent.parent
+
+proc = subprocess.run(
+    ["python", "-m", "mddj", "read", "requires-python", "--lower-bound"],
+    check=True,
+    capture_output=True,
+    cwd=REPO_ROOT,
+)
+requires_python_version = proc.stdout.decode().strip()
+
+with open(REPO_ROOT / ".github" / "workflows" / "test.yaml") as f:
+    workflow = YAML.load(f)
+    includes = workflow["jobs"]["test"]["strategy"]["matrix"]["include"]
+    for include in includes:
+        if include["name"] == "Linux":
+            break
+    else:
+        raise ValueError("Could not find 'Linux' in the test matrix.")
+
+    for environment in include["tox-post-environments"]:
+        if environment.endswith("-mindeps"):
+            break
+    else:
+        raise ValueError("Could not find a '-mindeps' tox-post-environment.")
+
+    python_version, _, _ = environment.partition("-")
+    if python_version != f"py{requires_python_version}":
+        print("ERROR: ensure_min_python_is_tested.py failed!")
+        print(
+            f"\nPackage data sets 'Requires-Python: >={requires_python_version}', "
+            f"but the test-mindeps job is configured to test '{python_version}'.\n",
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+
+proc = subprocess.run(
+    ["python", "-m", "mddj", "read", "tox", "min-version"],
+    check=True,
+    capture_output=True,
+    cwd=REPO_ROOT,
+)
+tox_min_python_version = proc.stdout.decode().strip()
+if tox_min_python_version != requires_python_version:
+    print("ERROR: ensure_min_python_is_tested.py failed!")
+    print(
+        f"\nPackage data sets 'Requires-Python: >={requires_python_version}', "
+        "but tox is configured to test with a minimum of "
+        f"'{tox_min_python_version}'.\n",
+        file=sys.stderr,
+    )
+    sys.exit(1)
