6ea7ad6192f5706885d3f707907d654dbb710c39
Stephen Rosen <sirosen@globus.org>
2025-08-04 15:33:31 -0500

    Apply suggestions from code review
    Co-authored-by: Kurt McKee <contactme@kurtmckee.org>

--- scripts/ensure_min_python_is_tested.py
+++ scripts/ensure_min_python_is_tested.py
@@ -8,38 +8,36 @@ import ruamel.yaml
 YAML = ruamel.yaml.YAML(typ="safe")
 REPO_ROOT = pathlib.Path(__file__).parent.parent
 
-proc = subprocess.run(
+requires_python_version = subprocess.check_output(
     ["python", "-m", "mddj", "read", "requires-python", "--lower-bound"],
-    check=True,
-    capture_output=True,
     cwd=REPO_ROOT,
-)
-requires_python_version = proc.stdout.decode().strip()
+    text=True,
+).strip()
 
-with open(REPO_ROOT / ".github" / "workflows" / "test.yaml") as f:
+with open(REPO_ROOT / ".github/workflows/test.yaml") as f:
     workflow = YAML.load(f)
-    includes = workflow["jobs"]["test"]["strategy"]["matrix"]["include"]
-    for include in includes:
-        if include["name"] == "Linux":
-            break
-    else:
-        raise ValueError("Could not find 'Linux' in the test matrix.")
+includes = workflow["jobs"]["test"]["strategy"]["matrix"]["include"]
+for include in includes:
+    if include["name"] == "Linux":
+        break
+else:
+    raise ValueError("Could not find 'Linux' in the test matrix.")
 
-    for environment in include["tox-post-environments"]:
-        if environment.endswith("-mindeps"):
-            break
-    else:
-        raise ValueError("Could not find a '-mindeps' tox-post-environment.")
+for environment in include["tox-post-environments"]:
+    if environment.endswith("-mindeps"):
+        break
+else:
+    raise ValueError("Could not find a '-mindeps' tox-post-environment.")
 
-    python_version, _, _ = environment.partition("-")
-    if python_version != f"py{requires_python_version}":
-        print("ERROR: ensure_min_python_is_tested.py failed!")
-        print(
-            f"\nPackage data sets 'Requires-Python: >={requires_python_version}', "
-            f"but the test-mindeps job is configured to test '{python_version}'.\n",
-            file=sys.stderr,
-        )
-        sys.exit(1)
+python_version, _, _ = environment.partition("-")
+if python_version != f"py{requires_python_version}":
+    print("ERROR: ensure_min_python_is_tested.py failed!")
+    print(
+        f"\nPackage data sets 'Requires-Python: >={requires_python_version}', "
+        f"but the test-mindeps job is configured to test '{python_version}'.\n",
+        file=sys.stderr,
+    )
+    sys.exit(1)
 
 
 proc = subprocess.run(
